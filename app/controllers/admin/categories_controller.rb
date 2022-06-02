59773074da23858ef412fd76e6c50be40b9e4f70
Charles Ngo <charlesngo37@gmail.com>
2022-06-02 10:23:33 -0700

    create admin/categories initial view & controller config completed

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,11 +1,28 @@
 class Admin::CategoriesController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+
   def index
+    @categories = Category.order(id: :desc).all
   end
 
   def new
+    @categories = Category.new
   end
 
   def create
+    @categories = Category.new(Category_params)
+
+    if @categories.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @categories = Category.find params[:id]
+    @categories.destroy
+    redirect_to [:admin, :category], notice: 'Category deleted!'
   end
 end
